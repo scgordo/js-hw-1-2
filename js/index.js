08afbba5e94daea6d7ec4714c56414ba1533ddfa
Scott Gordon <scgordon@gmail.com>
2017-12-12 14:05:59 -0800

    add files

---
+++ js/index.js
@@ -0,0 +1,10 @@
+var input1 = parseInt(prompt("Please enter the first number:"));
+
+var input2 = parseInt(prompt("Please enter the second number:"));
+
+var sum = input1 + input2;
+
+var product = input1 * input2;
+
+document.write(input1 + " + " + input2 + " = " + sum + "<br>");
+document.write(input1 + " * " + input2 + " = " + product);
